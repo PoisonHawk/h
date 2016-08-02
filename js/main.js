085cfffa9049b907afc91724820956061c194a40
PoiosnHawk <poisonhawk@yandex.ru>
2016-08-02 13:52:28 +0300

    back o top link

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
+
+function inWindow(s){
+  var scrollTop = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  var currentEls = $(s);
+  var result = [];
+  currentEls.each(function(){
+    var el = $(this);
+    var offset = el.offset();
+    if(scrollTop <= offset.top && (el.height() + offset.top) < (scrollTop + windowHeight))
+      result.push(this);
+  });
+  return $(result);
+}
+
+
+function onScroll(){
+  var boxesInWindow = inWindow("header#home");
+
+  if (boxesInWindow.length>0){
+    $('.up').hide();
+  } else {
+    $('.up').show();
+  }
+    // var scroll_top = $(document).scrollTop();
+    // $(".navbar a").each(function(){
+    //     var hash = $(this).attr("href");
+    //     var target = $(hash);
+    //
+    //     if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+    //         $(".navbar a.active").removeClass("active");
+    //         $(this).addClass("active");
+    //     } else {
+    //         $(this).removeClass("active");
+    //     }
+    // });
+
+
+};
+
+
+
 $(document).ready(function(){
 
-  $('.navbar a[href^="#"]').click(function(){
+  
+
+  $(document).on("scroll", onScroll);
+
+  $('.up a[href^="#"], .navbar a[href^="#"]').click(function(){
 
       var el = $(this).attr('href');
 
