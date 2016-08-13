2f80f7a9ed0ca10c5b5c2e65a9b2cc0cf3c6227c
PoiosnHawk <poisonhawk@yandex.ru>
2016-08-13 23:19:08 +0300

    catalof

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var imageResize = require('gulp-image-resize');
 var autoprefixer = require('gulp-autoprefixer');
+var uncss = require('gulp-uncss');
 
 gulp.task('default', function(){
 gulp.src('images/*')
@@ -25,5 +26,14 @@ gulp.src('images/partners/*')
 gulp.task('prefix', function(){
   return gulp.src('css/main.css')
     .pipe(autoprefixer())
+    .pipe(gulp.dest('css'))
+})
+
+
+gulp.task('uncss', function(){
+  return gulp.src('css/main.css')
+    .pipe(uncss({
+      html: ['index.html'],
+    }))
     .pipe(gulp.dest('css'))
 })
